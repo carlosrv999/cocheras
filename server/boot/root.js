d455e76fb964da4d9ba19f64bbc7ae5b419ff09a
carlosrv999 <carlosrv125@icloud.com>
2017-06-25 11:25:42 -0500

    cocheras comit

---
+++ server/boot/root.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function(app) {
+  var router = app.loopback.Router();
+  var Cliente = app.models.Cliente;
+  var Cochera = app.models.Cochera;
+  var DetalleMovServ = app.models.DetalleMovServ;
+  var Empleado = app.models.Empleado;
+  var Empresa = app.models.Empresa;
+  var MovimientoServicio = app.models.MovimientoServicio;
+  var ServicioCochera = app.models.ServicioCochera;
+  var TipoServicio = app.models.TipoServicio;
+
+  router.get('/', app.loopback.status());
+
+  router.get('/todaCocheraConServicios', function(req, res){
+    var idCochera = req.query.idCochera;
+    var servicios = {};
+
+    Cochera.find({
+      include: {
+        relation: 'servicioCocheras',
+        scope: {
+          include: {
+            relation: 'tipoServicio',
+          }
+        }
+      }
+    }, function(err, obj) {
+      if(!err) return res.json(obj);
+      else return res.json({"no": "encontrado"});
+    });
+  });
+
+
+  app.use(router);
+};
